244a6e00710f25c3ca7486ebc407ab24321383ee
AlishaKausheen <alishakausheen@gmail.com>
2023-12-08 06:21:39 -0800

    path fixed

---
+++ index.js
@@ -0,0 +1,44 @@
+
+
+
+//node server that will handle socket io connection
+//const io = require('socket.io')(8000);
+
+//const users = {};
+//io.on is socket.io instance that will listen many instances
+//socket.on will be responsible for a particular instance
+//broadcast.emit will send broadcast to everyone except
+// that individual that he joined
+/*io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+    
+
+users[socket.id] = name;
+      socket.broadcast.emit('user-joined', name)
+    });   
+    socket.on('send', message => {
+        socket.broadcast.emit('recieve', { message: message, name: users[socket.id] })
+    });
+})*/
+const http = require('http');
+const express = require('express');
+const path = require('path')
+const { Server } = require("socket.io");
+
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+//Socket.io
+io.on('connection', (socket) => {
+   console.log("A new user has been added",socket.id)
+})
+
+app.use(express.static(path.resolve('./public')));
+app.get('/', (req, res) => {
+    return res.sendFile('/public/index.html');
+
+})
+server.listen(9000, () =>
+    console.log('Server started')
+)
---
+++ public/client.js
@@ -0,0 +1,12 @@
+
+
+
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector('.container');
+
+const name = prompt('Enter your name to join :');
+
+
+

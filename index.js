9466ba01fcaf11d945d4f68a64dcee3b40ede569
AlishaKausheen <alishakausheen@gmail.com>
2023-12-08 22:21:18 -0800

    error in socket io

--- index.js
+++ index.js
@@ -29,16 +29,17 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+
+app.use(express.static(path.resolve('./public')));
+app.get('/', (req, res) => {
+    return res.sendFile('/public/index.html');
+})
+
 //Socket.io
 io.on('connection', (socket) => {
    console.log("A new user has been added",socket.id)
 })
 
-app.use(express.static(path.resolve('./public')));
-app.get('/', (req, res) => {
-    return res.sendFile('/public/index.html');
-
-})
 server.listen(9000, () =>
     console.log('Server started')
 )
